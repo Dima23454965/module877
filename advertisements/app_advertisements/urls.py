05507cb274268c7374653eb3f790e874fe9e8431
Дмитрий <besogonovdima870@gmail.com>
2023-08-22 20:51:27 +0500

    комит

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, page1
+from .views import index, page1, top_sellers
 
 
 urlpatterns = [
     path('', index),
-    path('page1/', page1)
+    path('page1/', page1),
+    path('top-sellers/', top_sellers)
 ]
